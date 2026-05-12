6ffe2e13e57f212f079b5751c82666685a8e3063
AB-stack-cmd <abirsarkar123455@gmail.com>
2026-05-12 21:03:45 +0530

    chnaged the model of ai

--- backend/index.ts
+++ backend/index.ts
@@ -9,29 +9,42 @@ app.use(express.json());
 const port = process.env.PORT || 3000
 console.log(port)
 
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || "");
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+
 const client = tavily({ apiKey:process.env.TAVILY_API_KEY });
 if(!client){
-  console.error(`Error : ${client}`)
+  console.error(`Error : ${client}`)  
 }
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || "");
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 app.post('/preplexity_ask',async (req, res) => {
-  const { query } = req.body;
+
+  const { query } =  req.body.query;
+
   if(!query){
     return res.status(400).json({
       error:"empty query"
     })
   }
+  try{
+  // web search
+   const webSearch = await client.search(query , {
+    searchDepth:"advanced"
+  });
 
-  try {
-    // Response from Tavily
-    const searchResponse = await client.search(query , {
-      searchDepth:"advanced"
+  const webResult = webSearch.results; // result from trively
+
+  const { textStream } = streamText({
+      model: "google/gemini-2.5-flash",
+      prompt: 'Invent a new holiday and describe its traditions.',
     });
 
-    const context = searchResponse.results.map(r => r.content).join("\n\n");
+    for await (const textPart of textStream) {
+      process.stdout.write(textPart);
+    }
+
+    const context = webSearch.results.map(r => r.content).join("\n\n");// result content form the web search 
     const prompt = `You are a helpful assistant. Use the following search results to answer the user query: ${query}\n\nSearch Results:\n${context}`;
 
     const result = await model.generateContent(prompt);
@@ -40,12 +53,13 @@ app.post('/preplexity_ask',async (req, res) => {
 
     res.json({
       answer: text,
-      sources: searchResponse.results
+      sources: webSearch.results
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
+     res.status(500).json({ error: "Internal Server Error" });
   }
+
 })
 
 app.listen(port, () => {
