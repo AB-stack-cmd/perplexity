c11acf057535419f27bffd3701c1a07957a6d443
AB-stack-cmd <abirsarkar123455@gmail.com>
2026-05-31 14:17:59 +0530

    Added delete route

--- backend/api/remove.ts
+++ backend/api/remove.ts
@@ -1,1 +1,20 @@
+import express from "express";
+import type { Request , Response } from "express";
+import Validation from "../middleware";
+import { request } from "node:http";
+import { error } from "node:console";
+import prisma from "../db";
 
+const router = express()
+
+router.delete("/delete" ,  Validation , async(res:Response,req:Request)=>{
+    try{
+        const conversation = await prisma.conversation.findFirst()
+
+    }catch(e){
+        res.json({
+            Message : "unable to delete conversation",
+            error : e
+        })
+    }
+})
